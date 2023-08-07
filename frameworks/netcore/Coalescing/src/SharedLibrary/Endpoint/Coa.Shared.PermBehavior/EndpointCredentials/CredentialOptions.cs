758a596884077d90ae158fbf644c42af0d7d170a
wioniqle-q <wioniqle.q@gmail.com>
2023-08-07 20:06:49 +0300

    Minor bug fix.

--- frameworks/netcore/Coalescing/src/SharedLibrary/Endpoint/Coa.Shared.PermBehavior/EndpointCredentials/CredentialOptions.cs
+++ frameworks/netcore/Coalescing/src/SharedLibrary/Endpoint/Coa.Shared.PermBehavior/EndpointCredentials/CredentialOptions.cs
@@ -2,7 +2,7 @@ namespace Coa.Shared.PermBehavior.EndpointCredentials;
 
 public sealed class CredentialOptions
 {
-    public string SecretKey => null!;
-    public string Issuer => null!;
-    public string Audience => null!;
+    public string SecretKey { get; init; } = null!;
+    public string Issuer { get; init; } = null!;
+    public string Audience { get; init; } = null!;
 }
